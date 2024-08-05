f70488fdd097d965f945ed52949df1ac4ab21147
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-08-05 21:11:51 +0300

    mvvm template

---
+++ src/rush00.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace rush00.App.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/rush00.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace rush00.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
