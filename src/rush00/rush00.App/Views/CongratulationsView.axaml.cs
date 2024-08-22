4b8e7178f25cffbe1ac08fb12c52c8331a6ef31a
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-08-22 18:21:46 +0300

    aboba

--- src/rush00/rush00.App/Views/CongratulationsView.axaml.cs
+++ src/rush00/rush00.App/Views/CongratulationsView.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using rush00.App.ViewModels;
 
 namespace rush00.App.Views;
 
@@ -9,5 +12,14 @@ public partial class CongratulationsView : UserControl
     public CongratulationsView()
     {
         InitializeComponent();
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (DataContext is CongratulationsViewModel viewModel)
+        {
+            Console.WriteLine("Screen pressed");
+            viewModel.OnScreenPressed();
+        }
     }
 }
